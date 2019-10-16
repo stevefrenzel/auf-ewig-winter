5b1a2d613e0fa3212a14799bc2306afb03bffe79
Steve Frenzel <stevefrenzel@gmx.de>
2019-10-16 08:45:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
 
-    console.log('script.js is working');
+    console.log("%cAUF EWIG WINTER", "color: #ec1e2e; font-size:30px; font-family:monospace;");
     
 })();
