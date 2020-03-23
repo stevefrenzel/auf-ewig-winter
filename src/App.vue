3f99915d2f4b67a3213f64b59349a65c2bb756dd
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-23 12:23:36 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <b-container fluid class="main-container">
+    <video autoplay muted loop id="video">
+      <source src="./assets/dust.mp4" type="video/mp4" />
+    </video>
+    <b-container fluid id="main-container">
       <!-- LOGO -->
       <!-- Possible to set breakpoint for width with a class name? -->
       <b-row class="text-center" align-h="center">
-        <b-col class="column-green m-5" xs="12" md="8">
-          <figure>
-            <img
-              class="w-50"
-              fluid
-              src="./assets/logo.png"
-              alt="AUF EWIG WINTER LOGO"
-            />
+        <b-col class="my-5" xs="12" md="8">
+          <figure class="m-0">
+            <img fluid src="./assets/logo.png" alt="AUF EWIG WINTER LOGO" />
           </figure>
         </b-col>
       </b-row>
@@ -19,13 +17,9 @@
       <!-- NAVIGATION -->
       <nav>
         <b-row class="text-center" align-h="center" align-v="center">
-          <b-col
-            v-for="item in navigation"
-            :key="item"
-            class="column-red"
-            lg="2"
+          <b-col v-for="item in navigation" :key="item.title" sm="auto"
             ><a :href="item.link" rel="noopener noreferrer"
-              ><h2 class="m-0">{{ item.title }}</h2></a
+              ><h2 class="m-0 px-3 nav-item">{{ item.title }}</h2></a
             ></b-col
           >
         </b-row>
@@ -34,15 +28,14 @@
       <!-- SOCIAL MEDIA -->
       <!-- Possible to set breakpoint for size with a class name? -->
       <section>
-        <b-row class="text-center m-5" align-h="center" align-v="center">
-          <b-col
-            v-for="item in socialMedia"
-            :key="item"
-            class="column-green"
-            cols="3"
+        <b-row class="mt-5 text-center" align-h="center">
+          <b-col v-for="item in socialMedia" :key="item.title" cols="2"
             ><h3 class="m-0">
               <a :href="item.link" rel="noopener noreferrer">
-                <font-awesome-icon :icon="['fab', item.logo]" size="3x"
+                <font-awesome-icon
+                  class="icon"
+                  :icon="['fab', item.logo]"
+                  size="3x"
               /></a></h3
           ></b-col>
         </b-row>
@@ -50,11 +43,11 @@
 
       <!-- FOOTER -->
       <!-- How to make this footer stick to bottom? -->
-      <footer class="footer text-center">
+      <!-- <footer class="footer text-center">
         <b-row>
           <b-col>Coded with ♥️ by Steve Frenzel.</b-col>
         </b-row>
-      </footer>
+      </footer> -->
     </b-container>
   </div>
 </template>
@@ -89,10 +82,24 @@
 
 <style>
   @import url('https://fonts.googleapis.com/css?family=Fira+Sans+Extra+Condensed&display=swap');
+
+  /* GENERAL: */
   #app {
     font-family: 'Fira Sans Extra Condensed', sans-serif;
     height: 100vh;
+    overflow: scroll;
     background-color: lightblue;
+  }
+  #video {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%;
+    min-height: 100%;
+  }
+  .nav-item,
+  .icon {
+    color: #e8402a;
   }
   .column-red {
     background-color: lightcoral;
@@ -102,7 +109,32 @@
     background-color: lightgreen;
     border: 1px solid black;
   }
+  /* NAVIGATION: */
+
+  /* SOCIAL MEDIA: */
+
+  /* FOOTER: */
   .footer {
     background-color: lightcoral;
+  }
+
+  /* MEDIA QUERIES: */
+  @media only screen and (max-width: 600px) {
+    img {
+      width: 75%;
+    }
+    .icon {
+      font-size: 3rem;
+    }
+  }
+  @media only screen and (min-width: 600px) {
+    img {
+      width: 50%;
+    }
+  }
+  @media only screen and (min-width: 768px) {
+    img {
+      width: 50%;
+    }
   }
 </style>
