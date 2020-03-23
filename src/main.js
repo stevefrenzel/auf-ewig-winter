809b09ebd28ed5605677581afcc66ba23f2f9e7c
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-23 08:15:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ Vue.use(BootstrapVue);
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
+// FONT AWESOME
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBandcamp } from '@fortawesome/free-brands-svg-icons/faBandcamp';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(faBandcamp, faFacebook, faInstagram);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
