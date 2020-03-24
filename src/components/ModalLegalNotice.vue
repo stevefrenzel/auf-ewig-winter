76066e0dd39f1e71f3bc3eefe4c38feb06fc7066
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-24 14:45:23 +0100

    Create ModalLegalNotice.vue

---
+++ src/components/ModalLegalNotice.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-modal>
+    <h1>LEGAL NOTICE MODAL</h1>
+  </b-modal>
+</template>
+
+<script>
+  export default {
+    name: 'ModalLegalNotice'
+  };
+</script>
+
+<style scoped></style>
