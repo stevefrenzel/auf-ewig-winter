1df21b83d13c886d4e5d1d896db5a0dc5e257c28
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-26 11:33:39 +0100

    Update ModalArtists.vue

--- src/components/ModalArtists.vue
+++ src/components/ModalArtists.vue
@@ -1,12 +1,41 @@
 <template>
-  <b-modal>
-    <h1>ARTISTS MODAL</h1>
+  <b-modal
+    v-model="isVisible"
+    centered
+    hide-header
+    hide-footer
+    :body-bg-variant="bodyBgVariant"
+    :body-text-variant="bodyTextVariant"
+  >
+    <img src="../assets/kora-winter-logo.png" alt="" width="250" />
+    <img
+      src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=660&q=80"
+      alt=""
+      width="250"
+    />
   </b-modal>
 </template>
 
 <script>
   export default {
-    name: 'ModalArtists'
+    name: 'ModalArtists',
+    props: ['value'],
+    computed: {
+      isVisible: {
+        get() {
+          return this.value;
+        },
+        set(val) {
+          this.$emit('input', val);
+        }
+      }
+    },
+    data() {
+      return {
+        bodyBgVariant: 'dark',
+        bodyTextVariant: 'white'
+      };
+    }
   };
 </script>
 
