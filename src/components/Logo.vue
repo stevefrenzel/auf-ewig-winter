de7cecb31df1053748d9a9270db93250b719d074
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-27 11:31:01 +0100

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,13 +1,11 @@
 <template>
-  <transition name="fade">
-    <b-row class="text-center" align-h="center">
-      <b-col class="mb-5" xs="12" md="8">
-        <figure class="m-0">
-          <img fluid src="../assets/logo.png" alt="AUF EWIG WINTER LOGO" />
-        </figure>
-      </b-col>
-    </b-row>
-  </transition>
+  <b-row class="text-center" align-h="center">
+    <b-col class="mb-5" xs="12" md="8">
+      <figure class="m-0">
+        <img fluid src="../assets/logo.png" alt="AUF EWIG WINTER LOGO" />
+      </figure>
+    </b-col>
+  </b-row>
 </template>
 
 <script>
@@ -17,13 +15,6 @@
 </script>
 
 <style scoped>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0;
-  }
   @media only screen and (max-width: 600px) {
     img {
       width: 75%;
