332deecf5feefd072e0c013ace170882f8b3ec0d
Steve Frenzel <stevefrenzel@gmx.de>
2020-03-27 11:31:04 +0100

    Update ModalArtists.vue

--- src/components/ModalArtists.vue
+++ src/components/ModalArtists.vue
@@ -7,12 +7,18 @@
     :body-bg-variant="bodyBgVariant"
     :body-text-variant="bodyTextVariant"
   >
-    <img src="../assets/kora-winter-logo.png" alt="" width="250" />
-    <img
-      src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=660&q=80"
-      alt=""
-      width="250"
-    />
+    <b-container>
+      <b-row class="text-center">
+        <b-col class="my-4">
+          <a href="https://haxan030.bandcamp.com/" alt="HAXAN Bandcamp">
+            <img src="../assets/haxan-logo.png" alt="" width="250" /> </a
+        ></b-col>
+        <b-col class="my-4">
+          <a href="https://korawinter.bandcamp.com/" alt="Kora Winter Bandcamp"
+            ><img src="../assets/kora-winter-logo.png" alt="" width="250"/></a
+        ></b-col>
+      </b-row>
+    </b-container>
   </b-modal>
 </template>
 
@@ -39,4 +45,8 @@
   };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .test {
+    width: 5%;
+  }
+</style>
