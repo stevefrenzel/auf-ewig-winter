07c25ae9bf53b9365c51c7ff310858499f2e4576
Steve Frenzel <stevefrenzel@gmx.de>
2020-05-22 16:03:08 +0200

    Added icons

--- src/main.js
+++ src/main.js
@@ -14,11 +14,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faAmazon } from '@fortawesome/free-brands-svg-icons/faAmazon';
 import { faApple } from '@fortawesome/free-brands-svg-icons/faApple';
 import { faBandcamp } from '@fortawesome/free-brands-svg-icons/faBandcamp';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons/faFacebook';
 import { faGooglePlay } from '@fortawesome/free-brands-svg-icons/faGooglePlay';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faItunes } from '@fortawesome/free-brands-svg-icons/faItunes';
 import { faNapster } from '@fortawesome/free-brands-svg-icons/faNapster';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons/faShoppingCart';
 import { faSoundcloud } from '@fortawesome/free-brands-svg-icons/faSoundcloud';
 import { faSpotify } from '@fortawesome/free-brands-svg-icons/faSpotify';
 import { faYoutubeSquare } from '@fortawesome/free-brands-svg-icons/faYoutubeSquare';
@@ -27,11 +30,14 @@ library.add(
   faAmazon,
   faApple,
   faBandcamp,
+  faCaretDown,
+  faCaretUp,
   faFacebook,
   faGooglePlay,
   faInstagram,
   faItunes,
   faNapster,
+  faShoppingCart,
   faSoundcloud,
   faSpotify,
   faYoutubeSquare
