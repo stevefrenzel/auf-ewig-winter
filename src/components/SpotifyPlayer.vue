1873a8cebb9c566500b676236294f8b903ea441d
Steve Frenzel <stevefrenzel@gmx.de>
2020-05-31 11:06:09 +0200

    Create SpotifyPlayer.vue

---
+++ src/components/SpotifyPlayer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <iframe class="spotify-player" :src="url" frameborder="0" allowtransparency="true"></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SpotifyPlayer',
+    props: {
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  @media only screen and (max-width: 600px) {
+    .spotify-player {
+      /* display: none; */
+      width: 100%;
+      height: 210px;
+    }
+  }
+  @media only screen and (min-width: 600px) {
+    .spotify-player {
+      width: 520px;
+      height: 210px;
+    }
+  }
+  @media only screen and (min-width: 992px) {
+    .spotify-player {
+      width: 800px;
+      height: 210px;
+    }
+  }
+</style>
