8fb44bf9a06f95649cba88eafdc734230215ab6d
Steve Frenzel <stevefrenzel@gmx.de>
2020-05-31 11:06:11 +0200

    Create YouTubePlayer.vue

---
+++ src/components/YouTubePlayer.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <iframe
+      class="youtube-player"
+      :src="url"
+      frameborder="0"
+      allowfullscreen
+      scrolling="no"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'YouTubePlayer',
+    props: {
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  @media only screen and (max-width: 600px) {
+    .youtube-player {
+      width: 100%;
+    }
+  }
+  @media only screen and (min-width: 600px) {
+    .youtube-player {
+      width: 560px;
+      height: 315px;
+    }
+  }
+  @media only screen and (min-width: 992px) {
+    .youtube-player {
+      width: 840px;
+      height: 473px;
+    }
+  }
+</style>
