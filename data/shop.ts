f6a24116b80fad90377b12472b53f3c74ac8c7e3
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-01 08:54:57 +0200

    create file

---
+++ data/shop.ts
@@ -0,0 +1,187 @@
+const merch = [
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/17-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-hetzjagd-oversize-tee',
+    description: `KORA WINTER - "Hetzjagd" Oversize Tee`,
+  },
+  {
+    tag: 'aew',
+    image: '/../public/images/merch/16-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/auf-ewig-winter-embroided-hoodie',
+    description: `AUF EWIG WINTER - "EMBROIDED" Hoodie`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/merch/15-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/haxan-ammoniak-oversize-tee',
+    description: `HAXAN - "AMMONIAK" Oversize Tee `,
+  },
+  {
+    tag: 'aew',
+    image: '/../public/images/merch/14-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/auf-ewig-winter-embroided-tee',
+    description: `AUF EWIG WINTER - "EMBROIDED" Tee`,
+  },
+  {
+    tag: 'aew',
+    image: '/../public/images/merch/13-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/auf-ewig-winter-embroided-beanie',
+    description: `AUF EWIG WINTER - "EMBROIDED" Beanie`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/12-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-bitter-limited-edition-12-colored-vinyl',
+    description: `KORA WINTER - "BITTER" Limited Edition 12" Vinyl`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/11-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-bitter-album-cd',
+    description: `KORA WINTER - "BITTER" Album (CD)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/10-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-laokoon-tee',
+    description: `KORA WINTER - "LAOKOON" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/09-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-ouroboros-tee',
+    description: `KORA WINTER - "OUROBOROS" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/08-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-bitter-tee',
+    description: `KORA WINTER - "BITTER" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/07-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-aew-tee',
+    description: `KORA WINTER - "AEW" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/06-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-vanitas-tee',
+    description: `KORA WINTER - "Vanitas" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/05-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-horns-tee',
+    description: `KORA WINTER - "Horns" Tee`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/04-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-welk-ep-cd',
+    description: `KORA WINTER - "WELK" EP (CD)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/03-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-bl-ht-ep-cd',
+    description: `KORA WINTER - "BLÜHT" EP (CD)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/02-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-button-set',
+    description: `KORA WINTER - BUTTON SET`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/merch/01-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/merch/kora-winter-laokoon-poster',
+    description: `KORA WINTER - "LAOKOON" Poster`,
+  },
+];
+
+const music = [
+  {
+    tag: 'kora',
+    image: '/../public/images/music/13-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/hetzjagd-prof-wolfff-cover',
+    description: `Hetzjagd (Prof. Wolfff Cover)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/12-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/jan-josef-liefers-halt-dein-gottverdammtes-maul',
+    description: `Jan Josef Liefers, halt dein gottverdammtes Maul`,
+  },
+  {
+    tag: 'nola',
+    image: '/../public/images/music/11-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/bubble-wrap-single',
+    description: `bubble wrap (Single)`,
+  },
+  {
+    tag: 'nola',
+    image: '/../public/images/music/10-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/hardcore-died-im-still-alive-single',
+    description: `hardcore died & i'm still alive (Single)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/09-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/stre-single',
+    description: `STRE$$ (Single)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/08-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/ammoniak-single',
+    description: `AMMONIAK (Single)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/07-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/raw-war-instrumentals-album',
+    description: `RAW/WAR Instrumentals (Album)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/06-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/war-ep',
+    description: `WAR (EP)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/music/05-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/track/deine-freunde-kommen-alle-in-die-h-lle-kaputtmacher-session-single',
+    description: `Deine Freunde (kommen alle in die Hölle) | Kaputtmacher Session (Single)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/music/04-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/bitter-album',
+    description: `BITTER (Album)`,
+  },
+  {
+    tag: 'haxan',
+    image: '/../public/images/music/03-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/raw-ep',
+    description: `RAW (EP)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/music/02-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/welk-ep',
+    description: `Welk (EP)`,
+  },
+  {
+    tag: 'kora',
+    image: '/../public/images/music/01-min.jpeg',
+    url: 'https://aufewigwinter.bandcamp.com/album/bl-ht-ep',
+    description: `Blüht (EP)`,
+  },
+];
+
+export { merch, music };
