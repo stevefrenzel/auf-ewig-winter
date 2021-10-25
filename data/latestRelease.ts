28870535d9fc1bf8921573a0805aa92972536afa
Steve Frenzel <stevefrenzel@gmx.de>
2021-10-25 10:48:06 +0200

    delete files

--- data/latestRelease.ts
+++
@@ -1,9 +0,0 @@
-const video = {
-  title: 'Hetzjagd by Kora Winter',
-  description: 'Hetzjagd - 2021',
-  src: 'https://cdn.iframe.ly/jLpli7l?click_to_play=1',
-};
-const music = {};
-const merch = {};
-
-export { video, music, merch };
